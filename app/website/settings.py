1dbd61ebd7afdd3723c0be3a536110a68b675379
md manzer alam <tooshort9541@gmail.com>
2023-05-22 22:13:43 +0530

    modified docker files

--- app/website/settings.py
+++ app/website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "setmeinprod")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get("DJANGO_DEBUG", 0)))
 
-ALLOWED_HOSTS = [] if DEBUG else os.environ.get("DJANGO_ALLOWED_HOSTS").split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
